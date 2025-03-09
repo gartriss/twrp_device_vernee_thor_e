faa9361f36c05ba002962996d3b1c264ea349fdf
Gartriss Martinez <161498302+gartriss@users.noreply.github.com>
2025-03-09 20:04:43 +0300

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,7 +51,7 @@ TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
 TARGET_PREBUILT_KERNEL := device/vernee/thor_e/prebuilt/kernel
 
 # Partitions
